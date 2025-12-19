93ba8632dbd78250115554fa3d584d47958eafb9
Abraham Saenz <abraham.saenz@gmail.com>
2025-12-19 21:16:50 +0000

    Make it bigger, twice the size

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <span className="font-bold sm:inline-block">
+            <span className="font-bold sm:inline-block text-2xl">
               VA OMS
             </span>
           </Link>
