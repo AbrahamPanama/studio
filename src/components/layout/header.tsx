706268948e6f9e6cbb23769f213f03c9ac198a3b
Abraham Saenz <abraham.saenz@gmail.com>
2025-12-19 19:24:02 +0000

    here it is

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Package } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Header() {
   return (
@@ -7,9 +7,15 @@ export default function Header() {
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <Package className="h-6 w-6 text-primary" />
+            <Image
+              src="https://storage.googleapis.com/aifire.appspot.com/images%2Flogito.png"
+              alt="VA cards & crafts logo"
+              width={32}
+              height={32}
+              className="h-8 w-8"
+            />
             <span className="font-bold sm:inline-block">
-              OrderFlow
+              VA cards &amp; crafts
             </span>
           </Link>
         </div>
