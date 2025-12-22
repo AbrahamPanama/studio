2a612956a9897b32b9f91db669bb5400665ec4e5
Abraham Saenz <abraham.saenz@gmail.com>
2025-12-22 04:39:01 +0000

    /home/user/studio/public/logo.png

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import logo from '@/logo.png';
 
 export default function Header() {
   return (
@@ -8,13 +7,7 @@ export default function Header() {
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <Image
-                src={logo}
-                alt="VA OMS Logo"
-                width={40}
-                height={40}
-                className="h-10 w-10"
-            />
+            {/* Logo has been temporarily removed due to an issue with file generation. */}
             <span className="font-bold sm:inline-block text-2xl">
               VA OMS
             </span>
