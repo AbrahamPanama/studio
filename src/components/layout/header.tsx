e8a39c4fa575f6cfddabfd5448237d190b9200f1
Abraham Saenz <abraham.saenz@gmail.com>
2025-12-22 04:40:27 +0000

    Ok, ive created a folder called "public" and I've placed the image there

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            {/* Logo has been temporarily removed due to an issue with file generation. */}
+            <Image src="/logo.png" alt="VA OMS Logo" width={40} height={40} />
             <span className="font-bold sm:inline-block text-2xl">
               VA OMS
             </span>
