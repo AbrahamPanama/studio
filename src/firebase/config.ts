6a87c3753e683e750f87b3a6b85633abe6d5b981
Abraham Saenz <abraham.saenz@gmail.com>
2025-12-22 03:20:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "va-oms-copy-2-81096274-22694",
+  "appId": "1:1083307023410:web:12eedff593f3a57544b02b",
+  "apiKey": "AIzaSyCEC40awWDWY6TF3D1UnWg8HG4_MiyZ3c8",
+  "authDomain": "va-oms-copy-2-81096274-22694.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1083307023410"
+};
