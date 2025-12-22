9386ed3b94c245485e8d6018d49fed059036608d
Abraham Saenz <abraham.saenz@gmail.com>
2025-12-22 04:41:47 +0000

    Good, lets just add a bit of margin, a standard amount for it to look pr

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
+          <Link href="/" className="mr-6 flex items-center space-x-4">
             <Image src="/logo.png" alt="VA OMS Logo" width={40} height={40} />
             <span className="font-bold sm:inline-block text-2xl">
               VA OMS
