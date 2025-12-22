f76dc399fb76b9e96b1f45d2b647e756277446e7
Abraham Saenz <abraham.saenz@gmail.com>
2025-12-22 04:32:51 +0000

    Ok, lets add the logo /home/user/studio/src/logo.png to the title

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
   return (
@@ -6,6 +7,13 @@ export default function Header() {
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
+            <Image
+                src="/logo.png"
+                alt="VA OMS Logo"
+                width={40}
+                height={40}
+                className="h-10 w-10"
+            />
             <span className="font-bold sm:inline-block text-2xl">
               VA OMS
             </span>
