f97168b46ce2d3c075882581aaf9d1ea934d8265
Abraham Saenz <abraham.saenz@gmail.com>
2025-12-22 05:08:19 +0000

    Now, move the buttons closer to the form

--- src/components/orders/order-form.tsx
+++ src/components/orders/order-form.tsx
@@ -274,11 +274,11 @@ export function OrderForm({ order, formType }: OrderFormProps) {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <div className="container mx-auto py-10">
-          <div className="flex items-center space-x-2 mb-6">
+          <div className="flex items-center space-x-2 mb-4">
               <Image src="/logo.png" alt="VA Cards and Crafts Logo" width={60} height={60} />
               <h2 className="text-2xl font-bold">VA Cards and Crafts</h2>
           </div>
-          <div className="flex items-center justify-between mb-6">
+          <div className="flex items-center justify-between mb-4">
             <div>
                 <h1 className="text-2xl font-bold">{title}</h1>
                 {isEditing && <p className="text-sm text-muted-foreground">ID: {order?.id}</p>}
