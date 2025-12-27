f7545bdfbfa8179f82da181a9874e69d09fe3dba
Abraham Saenz <abraham.saenz@gmail.com>
2025-12-27 03:15:43 +0000

    the orders temain blank on my deployed web site

--- src/lib/seed-db.ts
+++ src/lib/seed-db.ts
@@ -17,19 +17,14 @@ export const seedDatabase = async (firestore: Firestore) => {
   }
 
   orders.forEach((order) => {
-    // Make sure to use the ID from the JSON file for the document ID
     const docRef = doc(ordersCollection, order.id);
     
     const data = {
         ...order,
-        // Convert string dates from JSON to Firestore Timestamps or Date objects
-        fechaIngreso: serverTimestamp(), // Set current date for seeded data
+        fechaIngreso: serverTimestamp(),
         entrega: new Date(order.entrega),
         entregaLimite: new Date(order.entregaLimite),
     };
-
-    // We remove the 'id' field from the data payload itself, as it's used as the doc key
-    delete (data as any).id;
 
     batch.set(docRef, data);
     count++;
