37d0a7fbdba53a6c285980cd852646a1b5cae06e
AbrahamPanama <abraham.saenz@gmail.com>
2026-01-14 00:02:16 -0500

    Employee Timekeeper Implementation 404 fix

--- src/app/(studio)/layout.tsx
+++ src/app/(studio)/layout.tsx
@@ -1,5 +1,5 @@
-import type {Metadata} from 'next';
-import './globals.css';
+import type { Metadata } from 'next';
+import '../globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import Header from '@/components/layout/header';
 import { FirebaseClientProvider } from '@/firebase';
